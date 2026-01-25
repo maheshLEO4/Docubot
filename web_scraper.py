ca80abd4121f756e6799c9bd9cff57b6cd75dcd7
Mahesh <maheshmahi5345@gmail.com>
2026-01-25 23:25:44 +0530

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -2,7 +2,7 @@ import re
 import time
 import requests
 from bs4 import BeautifulSoup
-from langchain.docstore.document import Document
+from langchain.schema import Document
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
